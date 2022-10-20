51dea64fd2f73cedb845cf57e295c3b5712c47da
Aiziji <aletv1501@gmail.com>
2022-10-20 18:29:33 -0400

    Implement aditional methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'user_id'
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
+  private
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
