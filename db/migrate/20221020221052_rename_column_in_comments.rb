a22feec8278cdb1200f42f941c2807a7d8669c45
Aiziji <aletv1501@gmail.com>
2022-10-20 18:21:28 -0400

    Create basic models

---
+++ db/migrate/20221020221052_rename_column_in_comments.rb
@@ -0,0 +1,6 @@
+class RenameColumnInComments < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :comments, :users_id, :user_id
+    rename_column :comments, :posts_id, :post_id
+  end
+end
