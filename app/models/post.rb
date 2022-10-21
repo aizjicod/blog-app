fd9ec6452589f67edd4d678f1b4de9c2d820f664
Aiziji <aletv1501@gmail.com>
2022-10-21 12:05:30 -0400

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   after_save :update_post_counter
+  validates :title, presence: true,  length: { maximum: 250,
+    too_long: " 250 characters is the maximum allowed" }
+  validates :comments_counter, presence: true,  numericality: { only_integer: true, :greater_than_or_equal_to 0 }
+  validates :likes_counter, presence: true,  numericality: { only_integer: true, :greater_than_or_equal_to 0 }
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
