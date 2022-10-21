94494e09616b59325a5c19ff1c711ae96bcf77c7
Aiziji <aletv1501@gmail.com>
2022-10-21 14:06:01 -0400

    Implement unit test for modals

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,17 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   after_save :update_post_counter
-  validates :title, presence: true,  length: { maximum: 250,
-    too_long: " 250 characters is the maximum allowed" }
-  validates :comments_counter, presence: true,  numericality: { only_integer: true, :greater_than_or_equal_to 0 }
-  validates :likes_counter, presence: true,  numericality: { only_integer: true, :greater_than_or_equal_to 0 }
+  validates :title, presence: true,  length: { maximum: 250, too_long: " 250 characters is the maximum allowed" }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  after_initialize :init
 
+  def init
+    return unless new_record?
+    self.likes_counter = 0
+    self.comments_counter = 0
+  end
+  
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
   validates :name, presence: true
-  validates :posts_counter,  numericality: { only_integer: true }
+  validates :posts_counter, numericality: { only_integer: true }
+  after_initialize :init
+
+  def init
+    return unless new_record?
+    self.posts_counter = 0
+  end
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
