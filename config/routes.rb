ac797377dc95c06b7a23ed3ca061bc9a8a8143d6
Aiziji <aletv1501@gmail.com>
2022-10-24 18:57:47 -0400

    Implement post controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get "/users/:id", to: "users#show"
-  get "/users", to: "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
