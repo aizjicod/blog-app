40597182c39b4a6c45aec7e33711ede5f9905294
Aiziji <aletv1501@gmail.com>
2022-10-25 18:55:23 -0400

    Fix linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,42 +1,42 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Posts response", :type => :request do
-  describe "/users/:id/posts" do
+RSpec.describe 'Posts response', type: :request do
+  describe '/users/:id/posts' do
     before(:example) { get '/users/:id/posts' }
 
-    it "get has correct status" do
+    it 'get has correct status' do
       expect(response).to have_http_status(200)
       expect(response).to have_http_status(:ok)
     end
-  
-    it "get renders correct template" do
+
+    it 'get renders correct template' do
       expect(response).to render_template(:index)
     end
-    it "get renders correct body" do 
+    it 'get renders correct body' do
       expect(response.body).to include("user's posts")
     end
-    
-    it "does not render a different template" do
+
+    it 'does not render a different template' do
       expect(response).to_not render_template(:show)
     end
   end
-  
-  describe "/users/:id/posts/:id" do
-    before(:example) {get "/users/:id/posts/:id"}
 
-    it "get has correct status" do
+  describe '/users/:id/posts/:id' do
+    before(:example) { get '/users/:id/posts/:id' }
+
+    it 'get has correct status' do
       expect(response).to have_http_status(200)
       expect(response).to have_http_status(:ok)
     end
-  
-    it "get renders correct template" do
+
+    it 'get renders correct template' do
       expect(response).to render_template(:show)
     end
-    it "get renders correct body" do 
+    it 'get renders correct body' do
       expect(response.body).to include("user's selected post")
     end
-    
-    it "does not render a different template" do
+
+    it 'does not render a different template' do
       expect(response).to_not render_template(:index)
     end
   end
