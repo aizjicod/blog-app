cfc01e11cd14c4f65f7644bd2e7edc4b3fd2468f
Aiziji <aletv1501@gmail.com>
2022-10-30 20:13:10 -0400

    Implement a log in mehod that for now returns true

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :log_in?
   def current_user
     User.first
+  end
+  def log_in?
+    true
   end
 end
