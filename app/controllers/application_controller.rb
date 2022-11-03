529183f01af5af5b6bd2798c3f641b15988e0122
Aiziji <aletv1501@gmail.com>
2022-11-03 16:23:46 -0400

    Add links to new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :log_in?
-  def current_user
-    User.first
-  end
+  helper_method :log_in?
+  protect_from_forgery with: :exception
 
+  before_action :update_allowed_parameters, if: :devise_controller?
   def log_in?
     false
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :photo, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :bio, :photo, :email, :password, :current_password)}
   end
 end
