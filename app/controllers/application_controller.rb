f6544725bf67cfe0fec3398add7dcf4cf0362a6e
Aiziji <aletv1501@gmail.com>
2022-11-03 22:13:17 -0400

    delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,13 @@
 class ApplicationController < ActionController::Base
-  helper_method :log_in?
-  protect_from_forgery with: :exception
-
+  # protect_from_forgery with: :exception
+  
+  before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
-  def log_in?
-    false
-  end
 
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :photo, :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :bio, :photo, :email, :password, :current_password)}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for  :users,
+  :path => '',
+  :path_names => {  :sign_in =>       'login',
+                    :sign_out =>      'logout',
+                    :sign_up =>       '',
+                    :registration =>  'register',
+                    :edit =>          'edit',
+                    :cancel =>        'cancel',
+                    :confirmation =>  'verification'  }
+
   root to: "users#index"
 
-  resources :users, only: [:index, :show] do
+  resources :users do
     resources :posts, only: [:index, :new, :create, :show] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
