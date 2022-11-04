ce26e0fb6151bcce445acc21082ef7e93336a32d
Mumenya Nyamu <48546992+Nyame-Wolf@users.noreply.github.com>
2022-11-04 20:01:40 +0300

    Fix email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
   # change to true to allow email to be sent during development
   
   # smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
